bd9127dac1388596a496907c7b9179410c0045a3
Nik Geh <nikgeh@gmail.com>
2011-07-20 23:18:04 -0700

    Created the UIView for drawing graphs. Blank view initially.

---
+++ Graphical Calculator/GraphView.h
@@ -0,0 +1,16 @@
+//
+//  GraphView.h
+//  Graphical Calculator
+//
+//  Created by Nik on 7/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GraphView : UIView {
+    
+}
+
+@end
